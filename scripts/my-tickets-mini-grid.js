eedd8b585f20eb7eb9b2fe91ab3dab5d6e898e87
Tristan Lee <tristanlee85@gmail.com>
2016-03-20 02:43:38 -0400

    Fixes issue with the owner filter being applied to the whole store and skewing results in the dashboard

--- scripts/my-tickets-mini-grid.js
+++ scripts/my-tickets-mini-grid.js
@@ -1,14 +1,43 @@
-Ext.define('Override.store.Tickets', {
-    override: 'Portal.store.Tickets',
+Ext.define('Override.view.ticket.ContainerController', {
+    override: 'Portal.view.ticket.ContainerController',
 
-    constructor: function (config) {
-        var me = this;
-        me.callParent([config]);
+    showFullGrid: function (view, info) {
+        var me = this,
+            fullGrid = view.getFullGrid(),
+            nextFilter = Sencha.nextFilter,
+            ticketView, store, cm, ownerCol;
 
-        me.addFilter({
-            property: 'owner',
-            operator: '=',
-            value:    Sencha.User.get('uid')
-        });
+        if (fullGrid) {
+            if (fullGrid.isComponent) {
+                if (fullGrid.ownerCt === view) {
+                    return;
+                }
+            } else {
+                ticketView = view.getTicketView();
+                if (ticketView && ticketView.isComponent) {
+                    store = Ext.getStore('ticketStore');
+                    store.blockLoad();
+                }
+                view.setFullGrid(fullGrid = Ext.create(fullGrid));
+
+                // Get the Assigned To column and apply the current user as
+                // a filter value if a value is not already set. This will
+                // set the current user as the initial filter so only their
+                // tickets are displayed. The filter can be removed from the
+                // grid to see all available tickets.
+                cm = fullGrid.getColumnManager();
+                ownerCol = cm.getHeaderByDataIndex('owner');
+                if (ownerCol && !ownerCol.filter.active) {
+                    ownerCol.filter.setValue(Sencha.User.get('uid'));
+                }
+
+                store && store.unblockLoad();
+            }
+            Ext.batchLayouts(function () {
+                //TODO would like to remove ticket view here
+                view.layout.setActiveItem(fullGrid);
+                me.handleNextFilter(nextFilter);
+            });
+        }
     }
 });
