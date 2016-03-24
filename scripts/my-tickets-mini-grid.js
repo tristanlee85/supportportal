f4d9219a7fe6e63c6ea9ef24f94fe8bdac20c74c
Tristan Lee <tristanlee85@gmail.com>
2016-03-23 20:16:15 -0400

    Fixes issue with ticket not able to closed

--- scripts/my-tickets-mini-grid.js
+++ scripts/my-tickets-mini-grid.js
@@ -5,34 +5,31 @@ Ext.define('Override.view.ticket.ContainerController', {
         var me = this,
             fullGrid = view.getFullGrid(),
             nextFilter = Sencha.nextFilter,
-            ticketView, store, cm, ownerCol;
-
+            ticketView = view.getTicketView(),
+            store, vm, cm, ownerCol;
         if (fullGrid) {
-            if (fullGrid.isComponent) {
-                if (fullGrid.ownerCt === view) {
-                    return;
-                }
-            } else {
-                ticketView = view.getTicketView();
-                if (ticketView && ticketView.isComponent) {
-                    store = Ext.getStore('ticketStore');
-                    store.blockLoad();
-                }
-                view.setFullGrid(fullGrid = Ext.create(fullGrid));
-
-                // Get the Assigned To column and apply the current user as
-                // a filter value if a value is not already set. This will
-                // set the current user as the initial filter so only their
-                // tickets are displayed. The filter can be removed from the
-                // grid to see all available tickets.
-                cm = fullGrid.getColumnManager();
-                ownerCol = cm.getHeaderByDataIndex('owner');
-                if (ownerCol && !ownerCol.filter.active) {
-                    ownerCol.filter.setValue(Sencha.User.get('uid'));
-                }
-
-                store && store.unblockLoad();
+            ticketView = view.getTicketView();
+            if (ticketView && ticketView.isComponent) {
+                store = Ext.getStore('ticketStore');
+                vm = ticketView.getViewModel();
+                vm.set('tid', null);
+                store.blockLoad();
             }
+
+            view.setFullGrid(fullGrid = Ext.create(fullGrid));
+
+            // Get the Assigned To column and apply the current user as
+            // a filter value if a value is not already set. This will
+            // set the current user as the initial filter so only their
+            // tickets are displayed. The filter can be removed from the
+            // grid to see all available tickets.
+            cm = fullGrid.getColumnManager();
+            ownerCol = cm.getHeaderByDataIndex('owner');
+            if (ownerCol && !ownerCol.filter.active) {
+                ownerCol.filter.setValue(Sencha.User.get('uid'));
+            }
+            
+            store && store.unblockLoad();
             Ext.batchLayouts(function () {
                 //TODO would like to remove ticket view here
                 view.layout.setActiveItem(fullGrid);
