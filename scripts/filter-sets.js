be1ac75efbd85be08e0f78fd17ac4b4b359f3708
Tristan Lee <tristanlee85@gmail.com>
2016-12-21 15:47:38 -0500

    Fixes: Grid filters setup causes app to crash when loading

--- scripts/filter-sets.js
+++ scripts/filter-sets.js
@@ -1,49 +1,49 @@
 Ext.define('Customization.view.filtersets.FilterSets', {
     extend: 'Ext.button.Button',
-    alias:  'widget.filtersetsbutton',
-
+    alias: 'widget.filtersetsbutton',
+    
     text: 'Filter Sets',
-    ui:   'blue-button',
-
+    ui: 'blue-button',
+    
     constructor: function (config) {
         var me = this,
             store = Ext.data.StoreManager.lookup('FilterSets');
-
+        
         me.callParent([config]);
-
+        
         if (store.isLoaded()) {
             me.initMenu();
         } else {
             store.on('load', me.initMenu, me);
         }
     },
-
+    
     initMenu: function () {
         var me = this,
             configUtil = Customization.util.Config,
             store = Ext.data.StoreManager.lookup('FilterSets'),
             items = [],
             defaultFilterSet = configUtil.getConfiguration('filter-sets', null);
-
+        
         defaultFilterSet = defaultFilterSet && defaultFilterSet.default || 0;
-
+        
         store.each(function (record) {
             items.push({
-                text:      record.get('name'),
-                record:    record,
+                text: record.get('name'),
+                record: record,
                 iconAlign: 'left',
-                iconCls:   Ext.String.format('x-fa fa{0}-square-o filterset-default', defaultFilterSet === record.getId() ? '-check' : '')
+                iconCls: Ext.String.format('x-fa fa{0}-square-o filterset-default', defaultFilterSet === record.getId() ? '-check' : '')
             })
         });
-
+        
         me.setMenu({
-            xtype:     'menu',
-            items:     items,
+            xtype: 'menu',
+            items: items,
             listeners: {
                 click: function (menu, item, e) {
                     var target = Ext.get(e.target),
                         record = item.record;
-
+                    
                     if (target.hasCls('filterset-default')) {
                         Customization.util.Config.setConfiguration('filter-sets', {default: record.getId()});
                         me.initMenu(Ext.data.StoreManager.lookup('FilterSets'));
@@ -59,56 +59,58 @@ Ext.define('Customization.view.filtersets.FilterSets', {
 
 Ext.define('Override.view.ticket.grid.Grid', {
     override: 'Portal.view.ticket.grid.Grid',
-
+    
     initComponent: function () {
         var me = this,
             configUtil = Customization.util.Config,
-            filterDock = me.getFilterDock(),
             defaultFilterSet = configUtil.getConfiguration('filter-sets', null),
             filterSetStore = Ext.data.StoreManager.lookup('FilterSets'),
             container;
-
+        
         me.callParent();
-
+        
+        me.dockedItems.remove(me.getFilterDock());
+        me.setFilterDock(null);
+        
         // create a new container to dock
         container = Ext.create({
-            xtype:  'container',
-            dock:   'top',
+            xtype: 'container',
+            dock: 'top',
             layout: {
-                type:     'box',
-                align:    'stretch',
+                type: 'box',
+                align: 'stretch',
                 vertical: me.getXType() === 'portal-ticket-grid-mini'
             },
-            items:  [filterDock, {
+            items: [{
                 xtype: 'filtersetsbutton'
+            }, {
+                xtype: 'gridfilters-dock',
+                store: me.getStore(),
+                flex: 1
             }]
         });
-
-        // remove the current instance of the gridfilters-dock component from the docked items
-        me.dockedItems.remove(filterDock);
-
+        
         // add it to the dock
-        filterDock.flex = 1;
         me.addDocked(container);
-
+        
         me.on('filtersetselect', me.onFilterSetSelect);
-
+        
         defaultFilterSet = defaultFilterSet && defaultFilterSet.default || 0;
-
+        
         // the filter set store may not be loaded yet
         filterSetStore.on('load', function () {
             var record = filterSetStore.findRecord('id', defaultFilterSet);
-
+            
             if (record) {
                 me.onFilterSetSelect(record);
             }
         }, filterSetStore, {single: true});
-
+        
         if (filterSetStore.isLoaded()) {
             filterSetStore.fireEvent('load');
         }
     },
-
+    
     onFilterSetSelect: function (record) {
         var me = this,
             store = me.getStore(),
@@ -116,7 +118,7 @@ Ext.define('Override.view.ticket.grid.Grid', {
             setFilters = record.get('filters'),
             newFilters = [],
             setFilter, value, dataIndex, filter, operator;
-
+        
         for (dataIndex in setFilters) {
             setFilter = setFilters[dataIndex];
             filter = filters[dataIndex];
@@ -139,7 +141,7 @@ Ext.define('Override.view.ticket.grid.Grid', {
                 newFilters.push({
                     operator: operator,
                     property: dataIndex,
-                    value:    value
+                    value: value
                 });
             }
         }
@@ -154,13 +156,13 @@ Ext.define('Override.view.ticket.grid.Grid', {
 
 Ext.define('Override.GridFilters.Dock', {
     override: 'GridFilters.Dock',
-
+    
     hide: function () {
         var me = this,
             grid = me.up('portal-ticket-grid-grid'),
             isMiniGrid = grid && grid.getXType() === 'portal-ticket-grid-mini',
             el = me.getEl();
-
+        
         // Hide the component only if it's the mini grid. Otherwise, just set the
         // element to be visibly hidden so that the layout is not modified.
         if (grid) {
@@ -171,13 +173,13 @@ Ext.define('Override.GridFilters.Dock', {
             }
         }
     },
-
+    
     show: function () {
         var me = this,
             grid = me.up('portal-ticket-grid-grid'),
             isMiniGrid = grid && grid.getXType() === 'portal-ticket-grid-mini',
             el = me.getEl();
-
+        
         // Show the component only if it's the mini grid. Otherwise, just set the
         // element to be visible so that the layout is not modified.
         if (grid) {
